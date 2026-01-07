2a1c6069aee0152471bb85b470b904a615c8212c
Gilson123tenemea <tenemeaaguilar@gmail.com>
2026-01-06 23:31:39 -0500

    conectar la app con firebase

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,14 +1,15 @@
 plugins {
     id("com.android.application")
     id("kotlin-android")
-    // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
+    id("com.google.gms.google-services")
+
 }
 
 android {
     namespace = "com.example.movil"
-    compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    compileSdk = 35
+    ndkVersion = "27.0.12077973"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
@@ -24,7 +25,7 @@ android {
         applicationId = "com.example.movil"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = flutter.minSdkVersion
+        minSdkVersion(23)
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
@@ -41,4 +42,12 @@ android {
 
 flutter {
     source = "../.."
+}
+
+dependencies {
+    // Firebase BoM
+    implementation(platform("com.google.firebase:firebase-bom:34.7.0"))
+
+    // Firebase Analytics (puedes quitarlo luego si no lo usas)
+    implementation("com.google.firebase:firebase-analytics")
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+
 import 'vistas/auth/login_page.dart';
 import 'vistas/supervisor/layout/supervisor_bottom_bar.dart';
 import 'vistas/inspector/layout/inspector_bottom_bar.dart';
 import 'vistas/trabajador/layout/trabajador_bottom_bar.dart';
 
-
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(MyApp());
 }
 
@@ -21,7 +27,6 @@ class MyApp extends StatelessWidget {
         "/supervisor/menu": (_) => const SupervisorBottomBar(),
         // Inspector
         "/inspector/menu": (_) => const InspectorBottomBar(),
-
         // Trabajador
         "/trabajador/menu": (_) => const TrabajadorBottomBar(),
       },
